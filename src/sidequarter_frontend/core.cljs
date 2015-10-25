92df76fca249bcaac6f5f33ac338d5b84b86dc40
Tolga Paksoy <tolga@hoppinger.com>
2015-10-25 15:09:27 +0100

    Add Om components for building a list of available sidekiqs

--- src/sidequarter_frontend/core.cljs
+++ src/sidequarter_frontend/core.cljs
@@ -1,6 +1,9 @@
 (ns ^:figwheel-always sidequarter-frontend.core
+    (:require-macros [cljs.core.async.macros :refer [go]])
     (:require[om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+             [om.dom :as dom :include-macros true]
+             [cljs.core.async :refer [<!]]
+             [sidequarter-frontend.api :as api]))
 
 (enable-console-print!)
 
@@ -8,13 +11,38 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {}))
+
+(defn sidekiq-view [data owner]
+  (om/component
+   (dom/div #js {:className "sidekiq" :id (data :id)}
+            (dom/h3 nil (data :namespace))
+            (dom/p nil (data :application))
+            (dom/p nil (data :redis_url)))))
+
+(defn sidekiq-list [data owner]
+  (om/component
+   (apply dom/div #js {:className "sidekiq-list"}
+          (om/build-all sidekiq-view (:sidekiqs data)))))
+
+(defn sidekiq-box [data owner]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (om/transact! data [:sidekiqs] (fn [] []))
+      (go
+        (let [response ((<! (api/get-sidekiqs)) :body)]
+          (om/update! data [:sidekiqs] (:sidekiqs response)))))
+    om/IRender
+    (render [_]
+      (dom/h1 nil "Sidekiqs")
+      (om/build sidekiq-list data))))
 
 (om/root
   (fn [data owner]
     (reify om/IRender
       (render [_]
-        (dom/h1 nil (:text data)))))
+        (dom/div nil (om/build sidekiq-box data)))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
@@ -24,4 +52,3 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
-
