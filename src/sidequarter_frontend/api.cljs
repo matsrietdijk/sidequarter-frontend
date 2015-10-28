b693ccec35e6664384b97b0f1740e08018c0a34f
Tolga Paksoy <tolga@hoppinger.com>
2015-10-28 21:53:00 +0100

    Implement API call to get stats of a sidekiq instance

--- src/sidequarter_frontend/api.cljs
+++ src/sidequarter_frontend/api.cljs
@@ -2,9 +2,17 @@
     (:require-macros [sidequarter-frontend.env :refer [cljs-env]]
                      [cljs.core.async.macros :refer [go]])
     (:require [cljs-http.client :as http]
-              [cljs.core.async :refer [<! >! put! chan]]))
+              [cljs.core.async :refer [<! >! put! chan]]
+              [clojure.string :refer [join]]))
 
 (def api-url (cljs-env :api-url))
 
 (defn get-sidekiqs []
   (http/jsonp api-url {:callback-name "callback"}))
+
+(defn stats-path [sidekiq-id]
+  (join "/" [api-url sidekiq-id "stats"]))
+
+(defn get-stats [sidekiq-id]
+  (let [url (stats-path sidekiq-id)]
+    (http/jsonp url {:callback-name "callback"})))
